6f4b648ba6dea4fc731dff78303e727b094a9610
AurelienNioche <nioche.aurelien@gmail.com>
2016-08-11 15:28:26 +0200

    Minor modifications for Avakas

---
+++ simple_main.py
@@ -0,0 +1,25 @@
+import numpy as np
+from multiprocessing import cpu_count
+from module.SimulationManager import Launcher
+from module.save_eco import BackUp
+
+
+def simple_run():
+
+    t_max = 10000
+    model = "BG"
+
+    root_folder = "../../example_data"
+
+    param = \
+        {
+            "workforce": np.array([50, 50, 100], dtype=int),
+            "t_max": t_max,  # Set the number of time units the simulation will run.
+            "cpu_count": cpu_count(),
+            "model": model,
+            "date": "example",
+            "idx": 0
+        }
+
+    results = Launcher.launch(param)
+    BackUp.save_data(results=results, parameters=param, root_folder=root_folder)
